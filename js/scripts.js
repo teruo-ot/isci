101d8329643368116c9a8fcb3ed63d3309cbc0df
teruo.ouchi <marcos.teruo.ouchi@gmail.com>
2021-11-06 17:47:21 -0300

    Atualização da estrutura inicial do site

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Função que permite o Include de html no html
+// Solução apresentada em https://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
+
+$(function () {
+    var includes = $('[data-include]')
+    $.each(includes, function () {
+        var file = 'views/' + $(this).data('include') + '.html'
+        $(this).load(file)
+    })
+})
